a3953457882553411a811cf84c1c49a734a748a1
Dosto Khan <dostokhan@gmail.com>
2017-04-19 14:37:09 +0600

    config symlink script added

---
+++ dotfiles/link-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'Deleting old files'
+rm $HOME/.zshrc $HOME/.vimrc.local $HOME/.vimrc.before.local $HOME/.vimrc.bundles.local $HOME/.editorconfig $HOME/.eslintrc
+
+echo 'Zshrc Config'
+ln -s $HOME/Work/dostokhan/utils/dotfiles/.zshrc $HOME/.zshrc
+
+echo 'vim Configs'
+ln -s $HOME/Work/dostokhan/utils/dotfiles/.vimrc.local $HOME/.vimrc.local
+ln -s $HOME/Work/dostokhan/utils/dotfiles/.vimrc.before.local $HOME/.vimrc.before.local
+ln -s $HOME/Work/dostokhan/utils/dotfiles/.vimrc.bundles.local $HOME/.vimrc.bundles.local
+ln -s $HOME/Work/dostokhan/utils/dotfiles/.editorconfig $HOME/.editorconfig
+
+echo 'Eslint'
+ln -s $HOME/Work/dostokhan/utils/dotfiles/.eslintrc $HOME/.eslintrc
