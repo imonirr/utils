c23977598d2e1de0f86f6ba0764b6100886f57fe
Moniruzzaman Monir <imonir.com@gmail.com>
2018-10-09 16:55:58 +0000

    i3 config properly symliked

--- dotfiles/link-config.sh
+++ dotfiles/link-config.sh
@@ -2,6 +2,7 @@
 
 echo 'Deleting old files'
 rm $HOME/.zshrc $HOME/.vimrc.local $HOME/.vimrc.before.local $HOME/.vimrc.bundles.local $HOME/.editorconfig $HOME/.eslintrc
+rm $HOME/.config/i3
 
 echo 'Zshrc Config'
 ln -s $HOME/Work/dostokhan/utils/dotfiles/.zshrc $HOME/.zshrc
@@ -16,4 +17,4 @@ echo 'Eslint'
 ln -s $HOME/Work/dostokhan/utils/dotfiles/.eslintrc $HOME/.eslintrc
 
 echo 'iconfig'
-ln -s $HOME/Work/dostokhan/utils/dotfiles/.i3 $HOME/.i3
+ln -s $HOME/Work/dostokhan/utils/dotfiles/.i3 $HOME/.config/i3
