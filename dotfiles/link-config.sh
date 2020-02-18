72de09e08e5c729acb3fb4e7bed397228b109f4b
Moniruzzaman Monir <monir@priyo.com>
2020-02-18 17:48:07 +0600

    configuration files updated. ale config added for vim

--- dotfiles/link-config.sh
+++ dotfiles/link-config.sh
@@ -5,17 +5,17 @@ rm $HOME/.zshrc $HOME/.vimrc.local $HOME/.vimrc.before.local $HOME/.vimrc.bundle
 rm -r $HOME/.config/i3
 
 echo 'Zshrc Config'
-ln -s $HOME/Work/dostokhan/utils/dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/Work/dostokhan/utils/dotfiles/.zprofile $HOME/.zprofile
+ln -s $HOME/work/utils/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/work/utils/dotfiles/.zprofile $HOME/.zprofile
 
 echo 'vim Configs'
-ln -s $HOME/Work/dostokhan/utils/dotfiles/.vimrc.local $HOME/.vimrc.local
-ln -s $HOME/Work/dostokhan/utils/dotfiles/.vimrc.before.local $HOME/.vimrc.before.local
-ln -s $HOME/Work/dostokhan/utils/dotfiles/.vimrc.bundles.local $HOME/.vimrc.bundles.local
-ln -s $HOME/Work/dostokhan/utils/dotfiles/.editorconfig $HOME/.editorconfig
+ln -s $HOME/work/utils/dotfiles/.vimrc.local $HOME/.vimrc.local
+ln -s $HOME/work/utils/dotfiles/.vimrc.before.local $HOME/.vimrc.before.local
+ln -s $HOME/work/utils/dotfiles/.vimrc.bundles.local $HOME/.vimrc.bundles.local
+ln -s $HOME/work/utils/dotfiles/.editorconfig $HOME/.editorconfig
 
 echo 'Eslint'
-ln -s $HOME/Work/dostokhan/utils/dotfiles/.eslintrc $HOME/.eslintrc
+ln -s $HOME/work/utils/dotfiles/.eslintrc $HOME/.eslintrc
 
 echo 'iconfig'
-ln -s $HOME/Work/dostokhan/utils/dotfiles/.i3 $HOME/.config/i3
+ln -s $HOME/work/utils/dotfiles/.i3 $HOME/.config/i3
